e3c5d218dcd4f7cfc0204ae287a42d53f55d3837
tonalmathew <tonalmathew.dev@gmail.com>
2022-08-11 13:29:58 +0530

    template complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Intro from './components/Intro/Intro'
+import About from './components/About/About'
+import Intrest from './components/Intrest/Intrest'
+import Footer from './components/Footer/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+      <About />
+      <Intrest />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Intrest/Intrest.jsx
@@ -0,0 +1,13 @@
+import './Intrest.css'
+
+function App() {
+
+  return (
+    <div className="Intrest">
+      <h3>Intrests</h3>
+      <p>Web Development</p>
+    </div>
+  )
+}
+
+export default App
