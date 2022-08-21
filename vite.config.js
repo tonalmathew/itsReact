d903f22ea9d016d34551a3368e89a6fb914f1658
tonalmathew <tonalmathew.dev@gmail.com>
2022-08-21 22:56:37 +0530

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      base: '/',
-      plugins: [react()]
-    }
-  } else {
-    return {
-      base: '/itsReact/',
-      plugins: [react()]
-    }
-  }
+export default defineConfig({
+  plugins: [react()]
 })
+
+// export default defineConfig(({ command }) => {
+//   if (command === 'serve') {
+//       return {
+//         base: '/',
+//         plugins: [react()]
+//       }
+//     } else {
+//       return {
+//         base: '/itsReact/',
+//         plugins: [react()]
+//       }
+//     }
+// })
