40d614dbca2426b3d13fb572f9bed8ec4b9f72ce
tonalmathew <tonalmathew.dev@gmail.com>
2022-08-21 19:18:05 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) =>(
-    {
-      base: command == 'build' ? '/itsReact/':'/',
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      base: '/',
       plugins: [react()]
-    })
-)
+    }
+  } else {
+    return {
+      base: '/itsReact/',
+      plugins: [react()]
+    }
+  }
+})
