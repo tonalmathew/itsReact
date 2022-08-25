a2ee7225e0ab43f82475f1f2fca4f3e6fc785404
tonalmathew <tonalmathew.dev@gmail.com>
2022-08-25 19:00:51 +0530

    update project 3

---
+++ src/components/Project3/Body/Body.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Body.css'
+// import memeData from '../../../../api/memeData'
+
+export default function Body() {
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg" 
+  })
+
+  function getMemeImage() {
+    
+  }
+
+  function inputData(event){
+    // console.log(event.target.value)
+    const {name, value} = event.target;
+    setMeme(prevMeme => (
+      {
+        ...prevMeme,
+        [name]: value
+      }
+    ))
+  }
+
+  return (
+    <div className='Body'>
+      <div className='form'>
+        <input 
+        type="text"
+        placeholder='Top Text' 
+        className='input'
+        name='topText'
+        value={meme.topText}
+        onChange={inputData}
+         />
+        <input 
+        type="text"
+        placeholder='Bottom Text' 
+        className='input'
+        name='bottomText'
+        value={meme.bottomText}
+        onChange={inputData}
+         />
+        <button className='button-submit' onClick={getMemeImage} type='submit'>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </div>
+  )
+}
--- src/pages/Project3.jsx
+++ src/pages/Project3.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Project3.css'
 import Navbar from '../components/Project3/Navbar/Navbar'
+import Body from '../components/Project3/Body/Body'
 
 export default function Project3() {
   return (
     <div className="Project3">
       <Navbar />
+      <Body />
     </div>
   )
 }
